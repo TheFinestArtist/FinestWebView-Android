eccd843cc839984583887f024ff08636147678ec
TheFinestArtist <liger745547@gmail.com>
2013-04-05 16:14:05 +0900

    Added git ignore

--- src/com/thefinestartist/simplewebviewfragment/SimpleWebViewFragment.java
+++ src/com/thefinestartist/simplewebviewfragment/SimpleWebViewFragment.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2013 The Finest Artist
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *limitations under the License.
+ */
 
 package com.thefinestartist.simplewebviewfragment;
 
@@ -31,7 +46,7 @@ import android.widget.RelativeLayout;
 public class SimpleWebViewFragment extends Fragment implements OnClickListener {
 
     public static final String EXTRA_URL = "url";
-    
+
     private String mUrl = null;
 
     private FrameLayout mView = null;
