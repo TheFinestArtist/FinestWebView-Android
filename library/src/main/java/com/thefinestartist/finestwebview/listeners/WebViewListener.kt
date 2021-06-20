8b2e487f3540c704b6f881cbe424783679f61ca6
Leonardo Taehwan Kim <leonardo@thefinestartist.com>
2021-06-20 01:16:33 -0400

    Convert WebViewListener from Java to Kotlin.

---
+++ library/src/main/java/com/thefinestartist/finestwebview/listeners/WebViewListener.kt
@@ -0,0 +1,21 @@
+package com.thefinestartist.finestwebview.listeners
+
+/**
+ * Created by TheFinestArtist on 1/26/16.
+ */
+abstract class WebViewListener {
+  fun onProgressChanged(progress: Int) {}
+  fun onReceivedTitle(title: String?) {}
+  fun onReceivedTouchIconUrl(url: String?, precomposed: Boolean) {}
+  fun onPageStarted(url: String?) {}
+  fun onPageFinished(url: String?) {}
+  fun onLoadResource(url: String?) {}
+  fun onPageCommitVisible(url: String?) {}
+  fun onDownloadStart(
+      url: String?,
+      userAgent: String?,
+      contentDisposition: String?,
+      mimeType: String?,
+      contentLength: Long
+  ) {}
+}
